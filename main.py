2dcd4c2e6541c652105c11534d9daf7c5bf3da22
Odin <odinuv@gmail.com>
2018-02-10 23:10:36 +0100

    fix: support input mapping

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 
+import csv
+
 print("Hello world from python")
+
+csvlt = '\n'
+csvdel = ','
+csvquo = '"'
+with open('/data/in/tables/source.csv', mode='rt', encoding='utf-8') as in_file:
+    lazy_lines = (line.replace('\0', '') for line in in_file)
+    reader = csv.DictReader(lazy_lines, lineterminator=csvlt, delimiter=csvdel, quotechar=csvquo)
+    for row in reader:
+        # do something
+        print("The first row is ", row)
+        # we don't want to print the entire file to output
+        exit() 
